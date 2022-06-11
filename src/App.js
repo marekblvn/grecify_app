d27042e3b57e306ac616bd05f90b6b9537be4046
Marek Balvín <marek267balvin@gmail.com>
2022-06-11 07:27:33 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import {Box, Typography} from "@mui/material";
+import { Box } from "@mui/material";
 
 import Title from "./components/Title";
 import Converter from "./components/Converter";
 
 export default function App() {
   return (
-    <Box sx={{
-      position: "absolute",
-      left: 0,
-      top: 0,
-      height: "100%",
-      width: "100%",
-      overflow: "auto",
-      backgroundColor: "#4F6367",
+    <Box
+      sx={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        height: "100%",
+        width: "100%",
+        overflow: "auto",
+        backgroundColor: "#4F6367",
         alignItems: "center",
-        justifyContent: "center"
-    }}>
-        <Title />
-        <Converter />
+        justifyContent: "center",
+      }}
+    >
+      <Title />
+      <Converter />
     </Box>
   );
 }
